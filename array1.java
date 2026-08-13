5999a95bd2b64fe234243f219368a0585692233c
Bhaskar <ombhaskar2252005@gmail.com>
2026-08-13 14:33:32 +0530

    add files

---
+++ array1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class array1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = {1,2,3,4,5};
+        System.out.println("Enter the number to search:");
+        int tar = sc.nextInt();
+        int n = arr.length;
+        for(int i=0; i<n; i++ ){
+            if(arr[i] == tar){
+                System.out.println("present");
+                return;
+            }
+        }
+        System.out.println("not present");
+
+    }
+}

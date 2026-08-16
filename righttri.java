797f416a6cf0c5cc372ca7292afa48a4f49cea10
Bhaskar <ombhaskar2252005@gmail.com>
2026-08-16 18:35:10 +0530

    add files

---
+++ righttri.java
@@ -0,0 +1,12 @@
+public class righttri {
+    public static void main(String aegs[]){
+        for(int i =0; i<5; i++){
+            for(int j =0; j<=i; j++){
+                System.out.print(" *");
+
+            }
+            System.out.println();
+        }
+        
+    }
+}

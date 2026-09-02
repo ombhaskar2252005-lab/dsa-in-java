48dbe0557b11da07a52e30d4d305ba9438edc383
Bhaskar <ombhaskar2252005@gmail.com>
2026-09-02 14:00:03 +0530

    adddd files

---
+++ ll1.java
@@ -0,0 +1,46 @@
+public class ll1 {
+    class Node{
+        int val;
+        Node next;
+        Node(int val){
+            this.val = val;
+            this.next = null;
+        }
+    }
+    public void print(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.val+"-->");
+            temp = temp.next;
+        }
+    }
+    public Node insertAtStart(Node head, int data){
+        Node n1 = new Node(data);
+        n1.next = head;
+        head = n1;
+        return head;
+    }
+    public void insertAtEnd(Node head, int data){
+        Node n2 = new Node(data);
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = n2;
+    }
+    public static void main(String[] args){
+        ll1 list = new ll1();
+        Node head = list.new Node(1);
+        head.next = list.new Node(2);
+        head.next.next = list.new Node(3);
+        Node n1 = list.insertAtStart(head, 50);
+        list.print(n1);
+        System.out.print("null");
+       
+        System.out.println();
+        list.insertAtEnd(head, 4);
+        list.print(head);
+        System.out.print("null");
+        System.out.println();
+    }
+}
